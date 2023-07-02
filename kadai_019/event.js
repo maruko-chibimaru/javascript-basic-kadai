334272abdf5f0952547fe7c422ff1ba920705711
maruko-chibimaru <136881382+maruko-chibimaru@users.noreply.github.com>
2023-07-02 19:46:14 +0900

    kadai_019

---
+++ kadai_019/event.js
@@ -0,0 +1,8 @@
+const outputBtn = document.getElementById('btn');
+const textText = document.getElementById('text');
+
+outputBtn.addEventListener('click', () =>{
+   
+    textText.textContent = 'ボタンをクリックしました';
+  
+});
